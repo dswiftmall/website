0e079de3569a445a3416d9a1d00bb69684197a72
dswiftmall <dswift.ltd@gmail.com>
2026-08-31 01:13:10 +0000

    Add files via upload

--- api/notify-admin.js
+++ api/notify-admin.js
@@ -53,7 +53,7 @@ export default async function handler(req, res) {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                from: 'D-Swift Mall <onboarding@resend.dev>', // swap once you add your own domain in Resend
+                from: 'D-Swift Mall <notifications@dswiftmall.com>',
                 to: [ADMIN_EMAIL],
                 subject: email.subject,
                 html: email.html,
